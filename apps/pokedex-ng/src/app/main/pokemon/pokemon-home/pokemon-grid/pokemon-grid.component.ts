0a48c6c4d20a7e7965463fde83b7917beea5d11b
Francisco Sanchez <frango9000@gmail.com>
2021-12-30 14:44:03 +0100

    fix multiple type damages dialogs

--- apps/pokedex-ng/src/app/main/pokemon/pokemon-home/pokemon-grid/pokemon-grid.component.ts
+++ apps/pokedex-ng/src/app/main/pokemon/pokemon-home/pokemon-grid/pokemon-grid.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { PxPokemon } from '@pokedex-ng/domain';
 import { MDBModalService } from 'angular-bootstrap-md';
-import { of } from 'rxjs';
+import { exhaustMap, of, Subject } from 'rxjs';
 import { PokemonService } from '../../../../shared/services/pokemon/pokemon.service';
 import { PokemonTypeDamagesComponent } from '../../pokemon-detail/pokemon-type-damages/pokemon-type-damages.component';
 
+@UntilDestroy()
 @Component({
   selector: 'app-pokemon-grid',
   templateUrl: './pokemon-grid.component.html',
@@ -13,14 +15,23 @@ import { PokemonTypeDamagesComponent } from '../../pokemon-detail/pokemon-type-d
 export class PokemonGridComponent {
   @Input() public pokemonList: PxPokemon[];
 
-  constructor(private readonly pokemonService: PokemonService, private readonly modalService: MDBModalService) {}
+  private readonly typeDamagesDialogClick: Subject<string | number> = new Subject<string | number>();
+
+  constructor(private readonly pokemonService: PokemonService, private readonly modalService: MDBModalService) {
+    this.typeDamagesDialogClick
+      .pipe(
+        untilDestroyed(this),
+        exhaustMap((pokemonId: string | number) => this.pokemonService.fetchApiOne(pokemonId))
+      )
+      .subscribe((pokemon) => {
+        this.modalService.show(PokemonTypeDamagesComponent, {
+          class: 'modal-dialog-centered',
+          data: { pokemon$: of(pokemon), showHeading: false },
+        });
+      });
+  }
 
   openTypeDamagesDialog(pokemonId: string | number) {
-    this.pokemonService.fetchApiOne(pokemonId).subscribe((pokemon) => {
-      this.modalService.show(PokemonTypeDamagesComponent, {
-        class: 'modal-dialog-centered',
-        data: { pokemon$: of(pokemon), showHeading: false },
-      });
-    });
+    this.typeDamagesDialogClick.next(pokemonId);
   }
 }
